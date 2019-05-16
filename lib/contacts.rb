2c24d23cd89e1ae26c7de1225d2e36535e124471
Jamie Vullo <jvullo@protonmail.com>
2019-05-16 13:52:04 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,8 @@ def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
-      binding.pry 
-   end
+        value.delete_if {|ice_cream|ice_cream == "strawberry"}
+        end
+      end
+    end
   end
-end
